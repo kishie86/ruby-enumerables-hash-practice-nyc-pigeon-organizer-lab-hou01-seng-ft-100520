4fc2e3f54f213871b5fa084f7028216088732825
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-23 19:41:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,8 +5,8 @@ final_results = data.each_with_object({}) do |(key, value), final_array|
 
   value.each do |inner_key, names|
     names.each do |name|
-        binding.pry
-      if !final_array[]
+        #binding.pry
+      if !final_array[name]
         final_array[name] = {}
     
     end
