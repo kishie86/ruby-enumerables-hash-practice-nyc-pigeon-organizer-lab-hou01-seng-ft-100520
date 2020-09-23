86df05b4f5ffd6cb857506f2178fe54494b941cd
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-23 19:47:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -14,7 +14,7 @@ final_results = data.each_with_object({}) do |(key, value), final_array|
        !final_array[name][key] = []
        end
       
-       final_array[name][key].push(inner_key)
+       final_array[name][key].push(inner_key.to_s)
        
       end
       #binding.pry
